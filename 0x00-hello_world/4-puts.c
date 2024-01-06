09c05c3204976527cc1f63bfd74464dc0c60ab4f
root <samareltayeb7526@gmail.com>
2024-01-06 07:40:10 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ *Return: Always 0.
+ */
+
 int main(void)
+
 {
-<TAB>puts("\"Programming is like building a multilingual puzzle");
-<TAB>return (0);
-}}
+    puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+}
